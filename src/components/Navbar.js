dfe4190269e7e0537e6288ad93d38a4391edaa72
uwo-o <hugo.campos@usm.cl>
2024-06-09 13:17:12 -0400

    padding added and tailwing animations added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ function Navbar() {
     return (
         <nav>
             <ul className="inline-flex flex-row fixed">
-                <li className="p-3 p-name">uwo.</li>
-                <li className="p-3"><a href="#home">Home</a></li>
-                <li className="p-3"><a href="#about">About</a></li>
-                <li className="p-3"><a href="#skill">Skills</a></li>
-                <li className="p-3"><a href="#career">Career</a></li>
-                <li className="p-3"><a href="#contact">Contact</a></li>
+                <li className="p-3 xl:px-5 p-name">uwo.</li>
+                <li className="p-3 xl:px-7"><a href="#home">Home</a></li>
+                <li className="p-3 xl:px-7"><a href="#about">About</a></li>
+                <li className="p-3 xl:px-7"><a href="#skill">Skills</a></li>
+                <li className="p-3 xl:px-7"><a href="#career">Career</a></li>
+                <li className="p-3 xl:px-7"><a href="#contact">Contact</a></li>
             </ul>
         </nav>
     )
