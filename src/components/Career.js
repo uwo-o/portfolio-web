57b41dd7b4ffc04793407b9eb2a660bd27cb26e2
uwo-o <hugo.campos@usm.cl>
2024-06-09 20:35:24 -0400

    navbar fixed and career section component created

---
+++ src/components/Career.js
@@ -0,0 +1,8 @@
+function Career () {
+    return(
+        <div id="career" className="container mx-auto flex items-center">
+        </div>
+    );
+}
+
+export default Career;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -38,14 +38,12 @@ function Navbar() {
     });
 
     return (
-        <nav className="z-10">
-            <ul className="inline-flex flex-row fixed">
+        <nav className="z-20">
+            <ul className="inline-flex flex-row fixed items-center">
                 <li className="p-3 xl:px-5 p-name">uwo.</li>
-                <li className="flex items-center" ><a className="p-3 xl:px-7" href="#home">Home</a></li>
-                <li className="flex items-center" ><a className="p-3 xl:px-7" href="#about">About</a></li>
-                <li className="flex items-center" ><a className="p-3 xl:px-7" href="#skill">Skills</a></li>
-                <li className="flex items-center" ><a className="p-3 xl:px-7" href="#career">Career</a></li>
-                <li className="flex items-center" ><a className="p-3 xl:px-7" href="#contact">Contact</a></li>
+                <li className="flex items-center" ><a className="flex items-center p-3 px-10" href="#home">Home</a></li>
+                <li className="flex items-center" ><a className="flex items-center p-3 px-10" href="#about">About</a></li>
+                <li className="flex items-center" ><a className="flex items-center p-3 px-10" href="#career">Career</a></li>
             </ul>
         </nav>
     )
